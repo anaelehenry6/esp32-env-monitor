84bcc039edc796d95c27f12bac8d337adaa9b17b
Henry Anaele <anaelehenry6@gmail.com>
2026-07-20 16:40:46 -0500

    milestone 3: MQ135 air quality sensor added to OLED display

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+#define MQ135_PIN 34
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_BME280 bme;
@@ -32,6 +33,10 @@ void setup() {
   }
   Serial.println("OLED ready!");
 
+  // MQ135 is analog, no initialization needed
+  pinMode(MQ135_PIN, INPUT);
+  Serial.println("MQ135 ready!");
+
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(1);
@@ -42,15 +47,32 @@ void setup() {
 }
 
 void loop() {
+  // BME280 readings
   float tempF = (bme.readTemperature() * 9.0 / 5.0) + 32.0;
   float humidity = bme.readHumidity();
   float pressure = bme.readPressure() / 100.0F;
 
+  // MQ135 reading (raw analog value 0-4095)
+  int airQuality = analogRead(MQ135_PIN);
+
+  // Determine air quality label
+  String airLabel;
+  if (airQuality < 1000) {
+    airLabel = "Good";
+  } else if (airQuality < 2000) {
+    airLabel = "Moderate";
+  } else if (airQuality < 3000) {
+    airLabel = "Poor";
+  } else {
+    airLabel = "Bad";
+  }
+
   // Print to Serial Monitor
   Serial.println("--------------------");
-  Serial.print("Temp:     "); Serial.print(tempF); Serial.println(" F");
-  Serial.print("Humidity: "); Serial.print(humidity); Serial.println(" %");
-  Serial.print("Pressure: "); Serial.print(pressure); Serial.println(" hPa");
+  Serial.print("Temp:      "); Serial.print(tempF); Serial.println(" F");
+  Serial.print("Humidity:  "); Serial.print(humidity); Serial.println(" %");
+  Serial.print("Pressure:  "); Serial.print(pressure); Serial.println(" hPa");
+  Serial.print("Air Quality: "); Serial.print(airQuality); Serial.print(" raw ("); Serial.print(airLabel); Serial.println(")");
 
   // Display on OLED
   display.clearDisplay();
@@ -62,23 +84,29 @@ void loop() {
   display.drawLine(0, 10, 127, 10, SSD1306_WHITE);
 
   // Temperature
-  display.setCursor(0, 14);
+  display.setCursor(0, 13);
   display.print("Temp:  ");
-  display.setTextSize(1);
   display.print(tempF, 1);
   display.println(" F");
 
   // Humidity
-  display.setCursor(0, 30);
+  display.setCursor(0, 23);
   display.print("Humid: ");
   display.print(humidity, 1);
   display.println(" %");
 
   // Pressure
-  display.setCursor(0, 46);
+  display.setCursor(0, 33);
   display.print("Press: ");
   display.print(pressure, 1);
   display.println(" hPa");
+
+  // Air Quality
+  display.setCursor(0, 43);
+  display.print("Air:   ");
+  display.print(airQuality);
+  display.print(" ");
+  display.println(airLabel);
 
   display.display();
 
